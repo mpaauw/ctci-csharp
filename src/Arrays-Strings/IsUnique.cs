35aa71e4a967563176bca19ff9f3af4997f899d7
mpaauw <matt.paauw@gmail.com>
2019-02-20 19:01:04 -0800

    Refactor dir structure; add namespaces; add initial shared code for 2.1.

---
+++ src/Arrays-Strings/IsUnique.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays_Strings
+{
+    public class IsUnique
+    {
+        public bool DoesStringConsistOfUniqueCharactersUsingAsciiBuffer(string input)
+        {
+            bool[] buffer = new bool[256];
+            for (int i = 0; i < input.Length; i++)
+            {
+                int asciiVal = input[i];
+                if (buffer[asciiVal])
+                {
+                    return false;
+                }
+                buffer[asciiVal] = true;
+            }
+            return true;
+        }
+
+        public bool DoesStringConsistOfUniqueCharacters(string input)
+        {
+            var arr = SortCharArray(input.ToCharArray());
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                char current = arr[i], next = arr[i + 1];
+                if (current == next)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private char[] SortCharArray(char[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = (char)temp;
+            }
+            return arr;
+        }
+    }
+}
---
+++ src/Linked-Lists/Common/SinglyLinkedListNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_Lists.Common
+{
+    public class SinglyLinkedListNode
+    {
+        public string Data { get; set; }
+
+        public SinglyLinkedListNode Next { get; set; }
+    }
+}
