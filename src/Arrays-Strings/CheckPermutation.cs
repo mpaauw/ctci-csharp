36622dacc886eb2e0e032916352096cafdeb255f
mpaauw <matt.paauw@gmail.com>
2019-02-24 15:33:46 -0800

    Add solution 1.2.

---
+++ src/Arrays-Strings/CheckPermutation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays_Strings
+{
+    public class CheckPermutation
+    {
+        public bool IsStringAPermutationOfAnother(string s1, string s2)
+        {
+            if(s1.Length != s2.Length)
+            {
+                return false;
+            }
+
+            int[] buffer = new int[256];
+            for(int i = 0; i < s1.Length; i++)
+            {
+                buffer[s1[i]] += 1;
+            }
+            for(int i = 0; i < s2.Length; i++)
+            {
+                buffer[s2[i]] -= 1;
+            }
+            for(int i = 0; i < buffer.Length; i++)
+            {
+                if(buffer[i] != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
