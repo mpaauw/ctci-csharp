2f2fe169a4d32451b281bfb132096c82d786644a
mpaauw <matt.paauw@gmail.com>
2019-02-26 19:05:50 -0800

    Add solution 2.3.

---
+++ src/Linked-Lists/DeleteMiddleNode.cs
@@ -0,0 +1,22 @@
+﻿using Linked_Lists.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_Lists
+{
+    public class DeleteMiddleNode
+    {
+        public SinglyLinkedListNode<T> DeleteMiddleNodeWithAccessOnlyToThatNode<T>(SinglyLinkedListNode<T> node)
+        {
+            if(node.Next == null)
+            {
+                throw new InvalidOperationException("Node supplied is not a 'middle' node.");
+            }
+            var nextNode = node.Next;
+            node.Data = nextNode.Data;
+            node.Next = nextNode.Next;
+            return node;
+        }
+    }
+}
