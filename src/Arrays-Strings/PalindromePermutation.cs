3de0bec80535d0675046b24600c88cbe3e940a5b
mpaauw <matt.paauw@gmail.com>
2019-03-21 20:54:04 -0700

    Add solution for 1.4.

---
+++ src/Arrays-Strings/PalindromePermutation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays_Strings
+{
+    public class PalindromePermutation
+    {
+        public bool IsPalindromePermutation(string input)
+        {
+            var characterCounts = new Dictionary<char, int>();
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if(characterCounts.ContainsKey(c))
+                {
+                    characterCounts[c] += 1;
+                }
+                else {
+                    characterCounts.Add(c, 1);
+                }
+            }
+
+            bool hasOddCount = false;
+            foreach(var key in characterCounts.Keys)
+            {
+                if(characterCounts[key] % 2 != 0)
+                {
+                    if(hasOddCount)
+                    {
+                        return false;
+                    }
+                    hasOddCount = true;
+                }
+            }
+            return true;
+        }
+    }
+}
