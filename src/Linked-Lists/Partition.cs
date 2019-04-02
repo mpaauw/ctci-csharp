599abfe48a3017c97077cf9721d4fdfb2b5f40a1
mpaauw <matt.paauw@gmail.com>
2019-04-01 18:28:06 -0700

    Add solution for 2.4.

---
+++ src/Linked-Lists/Partition.cs
@@ -0,0 +1,52 @@
+﻿using Linked_Lists.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_Lists
+{
+    public class Partition
+    {
+        SinglyLinkedListNode<int> lessThanList = null;
+        SinglyLinkedListNode<int> greaterThanEqualList = null;
+
+        public SinglyLinkedListNode<int> SortByPartition(int partition, SinglyLinkedListNode<int> list)
+        {
+            var lessPtr = lessThanList;
+            var greaterPtr = greaterThanEqualList;
+            var current = list;
+            while(current != null)
+            {
+                if(current.Data < partition)
+                {
+                    WriteToList(current.Data, lessThanList);
+                }
+                else
+                {
+                    WriteToList(current.Data, greaterThanEqualList);
+                }
+            }
+            lessThanList.Next = greaterPtr;
+            return lessPtr;
+        }
+
+        private void WriteToList(int value, SinglyLinkedListNode<int> list)
+        {
+            if(list is null)
+            {
+                list = new SinglyLinkedListNode<int>()
+                {
+                    Data = value
+                };
+            }
+            else
+            {
+                list.Next = new SinglyLinkedListNode<int>()
+                {
+                    Data = value
+                };
+                list = list.Next;
+            }
+        }
+    }
+}
