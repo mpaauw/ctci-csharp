32669442a347294a2f3e2e6561354b6e984ac156
mpaauw <matt.paauw@gmail.com>
2019-04-02 18:06:20 -0700

    Add solution for 4.4

---
+++ src/Trees-Graphs/CheckBalanced.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trees_Graphs.Common;
+
+namespace Trees_Graphs
+{
+    public class CheckBalanced<T>
+    {
+        public bool IsTreeBalanced(BinaryTreeNode<T> root)
+        {
+            if(Math.Abs(GetHeight(root.Left) - GetHeight(root.Right)) > 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private int GetHeight(BinaryTreeNode<T> node)
+        {
+            if(node is null)
+            {
+                return -1;
+            }
+            return Math.Max(GetHeight(node.Left), GetHeight(node.Right)) + 1;
+        }
+    }
+}
