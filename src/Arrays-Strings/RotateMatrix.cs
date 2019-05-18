0774c5abc3a0a48b2e97d46d511e524233f221ed
mpaauw <matt.paauw@gmail.com>
2019-05-18 16:29:20 -0700

    Add multiple cross-chapter solutions.

---
+++ src/Arrays-Strings/RotateMatrix.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays_Strings
+{
+    public class RotateMatrix
+    {
+        public int[][] RotateSquareMatrix(int[][] matrix)
+        {
+            int n = matrix.Length;
+
+            if (matrix.Length == 0 || matrix.Length != matrix[0].Length)
+            {
+                return null; // bad data
+            }
+
+            // go by layers, rotating cells one at a time per layer:
+            for (int layer = 0; layer < n / 2; layer++)
+            {
+                int first = layer;
+                int last = n - 1 - layer;
+                for (int i = first; i < last; i++)
+                {
+                    int offset = i - layer; // store offset for counting outwards => in on bottom and left
+
+                    int top = matrix[first][i]; // top
+                    int right = matrix[i][last]; // right
+                    int bottom = matrix[last][last - offset]; // bottom
+                    int left = matrix[last - offset][first]; // left
+
+                    // perform in-place swaps:
+                    matrix[first][i] = left; // left => top
+                    matrix[i][last] = top; // top => right;
+                    matrix[last][last - offset] = right; // right => bottom
+                    matrix[last - offset][first] = bottom; // bottom => left
+                }
+            }
+            return matrix; // return rotated matrix
+        }
+    }
+}
---
+++ src/Arrays-Strings/StringRotation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays_Strings
+{
+    public class StringRotation
+    {
+        public bool IsRotation(string s1, string s2)
+        {
+            int s1Length = s1.Length;
+            if (s1Length == s2.Length && s1.Length > 0)
+            {
+                string concatenatedS1 = s1 + s1;
+                return IsSubstring(concatenatedS1, s2);
+            }
+            return false;
+        }
+
+        private bool IsSubstring(string parent, string sub)
+        {
+            return parent.Contains(sub);
+        }
+    }
+}
