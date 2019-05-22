3137aa150ab79b533d543cc974ac1d3e2f987346
mpaauw <matt.paauw@gmail.com>
2019-05-22 08:49:44 -0700

    Add solution for 4.5.

---
+++ src/Trees-Graphs/ValidateBST.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trees_Graphs.Common;
+
+namespace Trees_Graphs
+{
+    public class ValidateBST
+    {
+        public bool IsBST(BinaryTreeNode<int> root)
+        {
+            return Recurse(root, Int32.MinValue, Int32.MaxValue);
+        }
+
+        private bool Recurse(BinaryTreeNode<int> node, int min, int max)
+        {
+            if(node == null)
+            {
+                return true;
+            }
+            if(node.Value <= min || node.Value > max)
+            {
+                return false;
+            }
+            return Recurse(node.Left, min, node.Value)
+                && Recurse(node.Right, node.Value, max);
+        }
+    }
+}
